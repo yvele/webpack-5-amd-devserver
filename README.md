887a2985f8cab27eabc37a29c14c9a1775d9bb29
yvele <merlicco@gmail.com>
2020-11-30 15:23:49 +0100

    chore: Initial commit
    Related to https://github.com/webpack/webpack/issues/12085

---
+++ README.md
@@ -0,0 +1,9 @@
+# webpack-5-amd-devserver
+
+```sh
+npm install
+npm run build
+npm run start
+```
+
+Open the console in your browser.
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  entry : path.join(__dirname, "src/main.js"),
+  output : {
+    path : path.join(__dirname, "dist"),
+    filename : "main.js",
+    libraryTarget : "amd",
+    libraryExport : "default"
+  }
+};
